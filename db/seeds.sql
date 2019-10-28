0b089adb412783c9f3e7bcb8db188df6601bd3a7
Max-M <maxnmagura@gmail.com>
2019-10-27 20:23:18 -0500

    set up connection and orm files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie burger", false);
