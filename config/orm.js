a17dc10ac141c62e1a9a897db722dbffed3fb818
Max-M <maxnmagura@gmail.com>
2019-10-27 23:52:06 -0500

    added in rest of files and starting route to test

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,30 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  var arr = [];
+r
+  for (var key in ob) {
+    var value = ob[key];
+   
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
 
 var orm = {
   selectAll: function(tableName) {
@@ -48,10 +74,5 @@ var orm = {
 
   }
 };
-
-
-
-
-
 
 module.exports = orm;
