10a3ba7b8c6d967d38f5ce3fde0d78bf060e75a6
Max-M <maxnmagura@gmail.com>
2019-10-31 19:11:30 -0500

    got the update function working
    clear

--- public/assets/js/burgerscript.js
+++ public/assets/js/burgerscript.js
@@ -1,25 +1,25 @@
 
 $(function() {
-  // $(".devour").on("click", function(event) {
-  //   console.log("HEY YOU ATE ME")
-  //   var id = $(this).data("id");
-  //   var isDevoured = $(this).data("isDevoured");
+  $(".devour").on("click", function(event) {
+    console.log("HEY YOU ATE ME")
+    var id = $(this).data("id");
+    var isDevoured = $(this).data("isDevoured");
 
-  //   var newDevourState = {
-  //     devoured: isDevoured
-  //   };
+    var newDevourState = {
+      devoured: isDevoured
+    };
 
-  //   $.ajax("api/burgers/" + id, {
-  //     type: "PUT",
-  //     data: newDevourState
-  //   }).then(
-  //     function() {
-  //       console.log("has been devoured", isDevoured);
+    $.ajax("api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("has been devoured", isDevoured);
 
-  //       location.reload();
-  //     }
-  //   );
-  // });
+        location.reload();
+      }
+    );
+  });
 
   $(".create-form").on("submit", function(event) {
   
